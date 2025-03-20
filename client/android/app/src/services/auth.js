4ea12ac1f112d9d0a642f1c6bc5130375f722ca4
mutty.schwarz <mutty320@gmail.com>
2025-03-20 21:14:42 +0200

    working on auth

--- client/android/app/src/services/auth.js
+++ client/android/app/src/services/auth.js
@@ -2,15 +2,20 @@ import { GoogleAuthProvider, signInWithCredential } from 'firebase/auth';
 import { auth } from './firebase';
 import * as Google from 'expo-auth-session/providers/google';
 import * as WebBrowser from 'expo-web-browser';
+import Constants from 'expo-constants';
 
 WebBrowser.maybeCompleteAuthSession();
+
+const GOOGLE_CLIENT_ID_WEB = Constants.expoConfig.extra.googleClientIdWeb;
+const GOOGLE_CLIENT_ID_ANDROID = Constants.expoConfig.extra.googleClientIdAndroid;
+const GOOGLE_CLIENT_ID_IOS = Constants.expoConfig.extra.googleClientIdIos;
 
 export const signInWithGoogle = async () => {
   try {
     const [request, response, promptAsync] = Google.useAuthRequest({
-      expoClientId: EXPO_PUBLIC_GOOGLE_CLIENT_ID_WEB,
-      androidClientId: EXPO_PUBLIC_GOOGLE_CLIENT_ID_ANDROID,
-      iosClientId: EXPO_PUBLIC_GOOGLE_CLIENT_ID_IOS,
+      expoClientId: GOOGLE_CLIENT_ID_WEB,
+      androidClientId: GOOGLE_CLIENT_ID_ANDROID,
+      iosClientId: GOOGLE_CLIENT_ID_IOS,
     });
 
     if (response?.type === 'success') {
