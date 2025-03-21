62fe21a25c8f823f596c4fd0eac6c4ef6bd3bd12
mutty.schwarz <mutty320@gmail.com>
2025-03-21 13:34:18 +0200

    chnged the app json, and the firebase js

--- client/src/services/firebase.js
+++ client/src/services/firebase.js
@@ -1,17 +1,20 @@
-// src/services/firebase.js
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: 'AIzaSyBfhUYKwDHiCy-3eKDNuUViDfLTbWS8gzA',
+  authDomain: 'triviaapp-454116.firebaseapp.com',
+  projectId: 'triviaapp-454116',
+  storageBucket: 'triviaapp-454116.appspot.com',
+  messagingSenderId: '908298948427',
+  appId: '1:908298948427:web:5a38b2eb9246291fa4e2ce',
+  measurementId: 'G-9KLYTGEG4J'
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
+
+export { app, auth };
