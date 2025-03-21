9ee9a3ebbe7e811994e91893cc86fa0888334c2f
mutty.schwarz <mutty320@gmail.com>
2025-03-21 15:17:02 +0200

    Fix: Track Firebase config files

--- client/android/app/src/main/java/com/triviaapp/MainActivity.kt
+++ client/android/app/src/main/java/com/triviaapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.goldmax.triviaapp
+package com.triviaapp
 
 import android.os.Build
 import android.os.Bundle
--- client/android/app/src/main/java/com/triviaapp/MainApplication.kt
+++ client/android/app/src/main/java/com/triviaapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.goldmax.triviaapp
+package com.triviaapp
 
 import android.app.Application
 import android.content.res.Configuration
